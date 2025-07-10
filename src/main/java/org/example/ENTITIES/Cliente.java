1c0509a34854fe08cf9542e3c4cc81539e562a7d
ToriLiv <oamossquintanilla@gmail.com>
2025-07-09 20:34:42 -0600

    feat: clase cliente

--- src/main/java/org/example/ENTITIES/Cliente.java
+++ src/main/java/org/example/ENTITIES/Cliente.java
@@ -2,4 +2,36 @@ package org.example.ENTITIES;
 
 public class Cliente {
     private String id;
+    private String nombre;
+    private String email;
+
+    public Cliente(String id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/org/example/EXCEPTIONS/ClienteDuplicado.java
@@ -0,0 +1,7 @@
+package org.example.EXCEPTIONS;
+
+public class ClienteDuplicado extends Exception {
+    public ClienteDuplicado(String message) {
+        super(message);
+    }
+}
